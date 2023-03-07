a5b564ca376c767337208b9cd8df3f8af3fbba6e
Bing Guo <bing.guo@som.umaryland.edu>
2023-03-07 10:17:57 -0500

    check fq files

--- env/run_snp_call_nf.py
+++ env/run_snp_call_nf.py
@@ -12,7 +12,7 @@ parser.add_argument(
     "--fqmap",
     type=str,
     default=None,
-    help="""path to the tsv table of input fastq files. The first row is ignored; 
+    help="""path to the tsv table of input fastq files. The first row is ignored;
     for the following lines, each should have 5 fields.
        col1: sample name of the fastq file;
        col2: an index of host species, use 0 if samples are isolated from human;
@@ -26,20 +26,28 @@ parser.add_argument(
 parser.add_argument(
     "--ena", action="store_true", help="when set, run pipeline on 6 real WGS samples"
 )
+parser.add_argument(
+    "--dry_run",
+    action="store_true",
+    help="when set, check input and print cmd without actually run the pipeline",
+)
 
 
 def check_fastq_map(fn):
     with open(fn, "r") as f:
-        for line in f:
+        for i, line in enumerate(f):
+            if i==0:
+                continue
             fields = line.rstrip().split("\t")
             for e in fields:
                 assert e != ""
             assert len(fields) == 5
-            sample = fields[0]
-            hostid = fields[1]
-            runid = fields[2]
-            mateid = fields[3]
+            # sample = fields[0]
+            # hostid = fields[1]
+            # runid = fields[2]
+            # mateid = fields[3]
             path = fields[4]
+            assert Path(path).exists()
 
 
 def lnk_testdata_fastq_map():
@@ -58,7 +66,7 @@ def lnk_testdata_fastq_map():
 def lnk_ena_fastq_map():
     dir = "/local/data/Malaria/Projects/Takala-Harrison/AFRIMS/snp_call_nf/"
     fn = f"{dir}/ena_data"
-        
+
     if Path("ena_data").exists():
         Path("ena_data").unlink()
     Path("ena_data").symlink_to(fn, target_is_directory=True)
@@ -69,7 +77,8 @@ def lnk_ena_fastq_map():
     return "fastq_map.tsv"
 
 
-def run_nextflow(fn):
+def run_nextflow(fn, dry_run=False):
+    check_fastq_map(fn)
     root = "/local/data/Malaria/Projects/Takala-Harrison/AFRIMS"
     cmd = f"""
     eval "$({root}/miniconda3/bin/conda shell.bash hook)"
@@ -78,23 +87,25 @@ def run_nextflow(fn):
             --gatk_tmpdir /local/data/Malaria/Projects/Takala-Harrison/AFRIMS/tmp
     """
     print(cmd)
-    assert run(cmd, shell=True).returncode == 0
+    if not dry_run:
+        assert run(cmd, shell=True).returncode == 0
 
 
 if __name__ == "__main__":
     args = parser.parse_args()
     fn = args.fqmap
-    
+
     if fn is None and not args.test and not args.ena:
         print(
-            "if fqmap not specified, either test or ena flag should be set", file=sys.stderr
+            "if fqmap not specified, either test or ena flag should be set",
+            file=sys.stderr,
         )
         sys.exit(-1)
     elif fn is None and args.test:
         fn = lnk_testdata_fastq_map()
-        run_nextflow(fn)
+        run_nextflow(fn, dry_run=args.dry_run)
     elif fn is None and args.ena:
         fn = lnk_ena_fastq_map()
-        run_nextflow(fn)
+        run_nextflow(fn, dry_run=args.dry_run)
     else:
-        run_nextflow(fn)
+        run_nextflow(fn, dry_run=args.dry_run)
